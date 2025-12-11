7b763b9464afe815b88e20550dc3dd7dc6410d16
techieshruti <shrutichandra721@gmail.com>
2025-12-11 13:44:49 +0530

    Added basic structure to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Sidebar from './components/layout/Sidebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      {/* LOGO */}
+      <div>360Cario</div>
+
+      {/* NAVIGATION BAR */}
+      <nav>
+        <ui>
+          <li>Dashboard</li>
+          <li>Email Lists</li>
+          <li>Email Accounts</li>  
+          <li>Email Compaign</li>  
+          <li>Analytics</li>  
+        </ui>
+      </nav>
+
+      {/* LOGIN iCON */}
+      <div>Login Icon</div>
+
+    </div>
   )
 }
 
