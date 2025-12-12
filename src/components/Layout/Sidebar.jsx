9e98473f50b59ef31d3d0c6cfaeb0a100bd93fca
techieshruti <shrutichandra721@gmail.com>
2025-12-12 15:44:35 +0530

    fixed sicebar logo

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -3,34 +3,39 @@ import { House, Users, Mail, Send, ChartColumn, LogIn } from 'lucide-react';
 
 const Sidebar = () => {
   return (
-    <div className='bg-[#e1eaf7] flex flex-col justify-between p-4 w-60 rounded-tr-3xl rounded-br-3xl'>
-      {/* logo */}
-      <div className="logo flex items-center gap-2">
+    <div className='bg-[#e1e9f2] h-screen flex flex-col justify-between p-4 w-60 rounded-tr-3xl rounded-br-3xl'>
+     
+      <div className='flex flex-col gap-3'>
+         {/* logo */}
+        <div className="logo flex items-center gap-2 m-4">
         <div className='bg-[#202c40] rounded-2xl p-2 text-white'><Mail /></div>
         <h1 className='text-2xl font-bold'>360ario</h1>
       </div>
 
       {/* navbar list */}
-      <div className="nav-list flex flex-col gap-4 mt-8">
-        <a href="#dashboard" className="nav-item flex gap-3 hover:bg-red-400 px-4 py-2 rounded-2xl">
-          <House /> Dashboard
+      <div className="nav-list text-lg flex flex-col gap-4 mt-8">
+        <a href="#dashboard" className="nav-item flex gap-3 focus:bg-[#ceddf1] text-gray-600 focus:font-semibold px-6 py-2 rounded-2xl">
+          <House className='focus:text-blue-600'/> Dashboard
         </a>
-        <a href="#profile" className="nav-item">
+        <a href="#profile" className="nav-item flex gap-3 focus:bg-[#ceddf1] text-gray-600 focus:font-semibold px-6 py-2 rounded-2xl">
           <Users /> Email List
         </a>
-        <a href="#settings" className="nav-item">
+        <a href="#settings" className="nav-item flex gap-3 focus:bg-[#ceddf1] text-gray-600 focus:font-semibold px-6 py-2 rounded-2xl">
           <Mail/> Email Accounts
         </a>
-        <a href="#settings" className="nav-item">
+        <a href="#settings" className="nav-item flex gap-3 focus:bg-[#ceddf1] text-gray-600 focus:font-semibold px-6 py-2 rounded-2xl">
           <Send /> Email Compaigns
         </a>
-        <a href="#settings" className="nav-item">
+        <a href="#settings" className="nav-item flex gap-3 focus:bg-[#ceddf1] text-gray-600 focus:font-semibold px-6 py-2 rounded-2xl">
           <ChartColumn /> Analytics
         </a>
       </div>
+      </div>
+      
+
 
       {/* login icon */}
-      <div className="login-icon">
+      <div className="login-icon flex mt-8 p-2 text-gray-600 hover:bg-[#ceddf1] cursor-pointer">
         <LogIn />
       </div>
     </div>
