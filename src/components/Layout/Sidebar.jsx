854c55e328af5545be4d886c50d70300d1e9bf88
techieshruti <shrutichandra721@gmail.com>
2025-12-12 15:11:54 +0530

    fixed sicebar logo

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -3,9 +3,12 @@ import { House, Users, Mail, Send, ChartColumn, LogIn } from 'lucide-react';
 
 const Sidebar = () => {
   return (
-    <div className='bg-blue-200 h-screen flex flex-col justify-between p-4 w-60'>
+    <div className='bg-[#e1eaf7] flex flex-col justify-between p-4 w-60 rounded-tr-3xl rounded-br-3xl'>
       {/* logo */}
-      <div className="logo">360ario</div>
+      <div className="logo flex items-center gap-2">
+        <div className='bg-[#202c40] rounded-2xl p-2 text-white'><Mail /></div>
+        <h1 className='text-2xl font-bold'>360ario</h1>
+      </div>
 
       {/* navbar list */}
       <div className="nav-list flex flex-col gap-4 mt-8">
