1ec0e835dd7d67cd9486874a1ed93be8f1977ce4
techieshruti <shrutichandra721@gmail.com>
2025-12-12 16:25:35 +0530

    added styles to sidebar

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 import { House, Users, Mail, Send, ChartColumn, LogIn } from 'lucide-react';
 
 const navItems = [
-  { id: "dashboard", label: "Dashboard", icon: House, color: "#2563eb" },
-  { id: "emailList", label: "Email List", icon: Users, color: "#10b981" },
-  { id: "emailAccounts", label: "Email Accounts", icon: Mail, color: "#f59e0b" },
-  { id: "campaigns", label: "Email Campaigns", icon: Send, color: "#ef4444" },
-  { id: "analytics", label: "Analytics", icon: ChartColumn, color: "#6366f1" },
+  { id: "dashboard", label: "Dashboard", icon: House, color: "#2563eb", bgColor: "#e3eefc" },
+  { id: "emailList", label: "Email List", icon: Users, color: "#10b981", bgColor: "#dcfae0" },
+  { id: "emailAccounts", label: "Email Accounts", icon: Mail, color: "#f59e0b", bgColor: "#faf2d4" },
+  { id: "campaigns", label: "Email Campaigns", icon: Send, color: "#ef4444", bgColor: "#fae6e6" },
+  { id: "analytics", label: "Analytics", icon: ChartColumn, color: "#6366f1", bgColor: "#e0e7ff" },
 ];
 
 const Sidebar = () => {
   const [activeItem, setActiveItem] = useState("dashboard");
 
   return (
-    <div className='bg-[#e1e9f2] h-screen flex flex-col justify-between fixed p-4 w-60 rounded-tr-3xl rounded-br-3xl'>
+    <div className='bg-[#f1f5fa] h-screen flex flex-col justify-between fixed p-4 w-60 rounded-tr-3xl rounded-br-3xl'>
 
       {/* Logo */}
       <div className='flex flex-col gap-3'>
@@ -32,8 +32,11 @@ const Sidebar = () => {
               <button
                 key={item.id}
                 onClick={() => setActiveItem(item.id)}
+                style={{
+                  background: isActive ? item.bgColor : "transparent"
+                }}
                 className={`flex items-center gap-3 px-6 py-2 rounded-2xl transition-all
-                  ${isActive ? "bg-[#ceddf1] font-semibold text-gray-800" : "text-gray-600 hover:bg-[#d9e4f2]"}
+                  ${isActive ? "font-semibold text-gray-800" : "text-gray-600 hover:bg-[#d9e4f2]"}
                 `}
               >
                 <Icon
