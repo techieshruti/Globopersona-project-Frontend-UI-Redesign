d274d4d39793690b8623d868e73b7c54d306d47b
techieshruti <shrutichandra721@gmail.com>
2025-12-12 15:04:03 +0530

    added lucide icons into navbar

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
+import { House, Users, Mail, Send, ChartColumn, LogIn } from 'lucide-react';
 
 const Sidebar = () => {
   return (
     <div className='bg-blue-200 h-screen flex flex-col justify-between p-4 w-60'>
       {/* logo */}
-      <div className="logo">360Cario</div>
+      <div className="logo">360ario</div>
 
       {/* navbar list */}
       <div className="nav-list flex flex-col gap-4 mt-8">
-        <a href="#dashboard" className="nav-item">
-          <i className="icon-dashboard"></i> Dashboard
+        <a href="#dashboard" className="nav-item flex gap-3 hover:bg-red-400 px-4 py-2 rounded-2xl">
+          <House /> Dashboard
         </a>
         <a href="#profile" className="nav-item">
-          <i className="icon-user"></i> Email List
+          <Users /> Email List
         </a>
         <a href="#settings" className="nav-item">
-          <i className="icon-settings"></i> Email Accounts
+          <Mail/> Email Accounts
         </a>
         <a href="#settings" className="nav-item">
-          <i className="icon-settings"></i> Email Compaigns
+          <Send /> Email Compaigns
         </a>
         <a href="#settings" className="nav-item">
-          <i className="icon-settings"></i> Analytics
+          <ChartColumn /> Analytics
         </a>
       </div>
 
       {/* login icon */}
       <div className="login-icon">
-        <i className="icon-login">login</i>
+        <LogIn />
       </div>
     </div>
   )
