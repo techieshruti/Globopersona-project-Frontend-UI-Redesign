8a8211a09d1f7ac8d618b0b8eac4e78705133b20
techieshruti <shrutichandra721@gmail.com>
2025-12-13 20:50:11 +0530

    added email layout

--- src/components/Email List/EmailLayoutCards.jsx
+++ src/components/Email List/EmailLayoutCards.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 const EmailLayoutCards = () => {
   return (
-    <div>
-        <div>
-            <input type="text" />search
+    <div className='ml-5 bg-slate-700 flex gap-2 justify-between'>
+        <div className=''>
+            <input 
+            className='pt-2'
+            type="text"
+            placeholder='Search lists by name, description, or tags...'/>
         <select name="">
             <option value="All Status">All Status</option>
         </select>
         <button>icon Filters</button>
         </div>
 
-        <div>
+        <div className='flex justify-center items-center gap-4'>
             <button>icon Grid</button>
             <h1>icon Lists</h1>
         </div>
