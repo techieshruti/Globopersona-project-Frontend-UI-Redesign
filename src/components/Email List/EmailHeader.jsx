d747f63bddb103573a8b1b1538965e2174275557
techieshruti <shrutichandra721@gmail.com>
2025-12-13 18:45:39 +0530

    added email header details

--- src/components/Email List/EmailHeader.jsx
+++ src/components/Email List/EmailHeader.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Database } from 'lucide-react'
 
 const EmailHeader = () => {
   return (
     <div className='p-4 flex justify-between items-center border-b-2 border-gray-200'>
         {/*Left */}
-        <div className='flex gap-4 items-center'>
-            <h1>icon</h1>
+        <div className='flex gap-3 items-center'>
+            <Database
+            size={40}
+            className='bg-gradient-to-br from-blue-500 via-indigo-500 to-purple-500 text-white p-2 rounded-2xl'
+            />
             <div>
-                <h1>Email List</h1>
-                <span>Welcome back, Ravi! Manage your email compaigns</span>
+                <h1 className="text-2xl font-bold">Email Lists</h1>
+                <span className='text-gray-400 font-semibold'>Welcome back, Ravi! Manage your email compaigns</span>
             </div>
         </div>
         {/* Right */}
