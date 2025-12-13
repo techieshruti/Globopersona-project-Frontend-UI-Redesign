c9bc52cb3f9feef4f61786e1e258e130e550e9b9
techieshruti <shrutichandra721@gmail.com>
2025-12-14 01:32:05 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter basename="/Globopersona-project-Frontend-UI-Redesign">
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
