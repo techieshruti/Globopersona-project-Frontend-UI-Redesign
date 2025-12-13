8b4842bcc419c6c64c4c61fbdf993d05f8f4fbd9
techieshruti <shrutichandra721@gmail.com>
2025-12-13 16:24:47 +0530

    added performance overview card

--- src/components/Dashboard/PerformanceOverview.jsx
+++ src/components/Dashboard/PerformanceOverview.jsx
@@ -7,18 +7,25 @@ const PerformanceOverview = () => {
         {/* Performance Overview heading */}
         <div>
             <h1 className='text-2xl font-bold'>PerformanceOverview</h1>
-            <p className='text-gray-400 font-semibold'>Track you email marketing matrics in real time</p>
+            <p className='text-gray-400 font-normal'>Track you email marketing matrics in real time</p>
         </div>
 
         {/* Performance Overview Card */}
-        <div className='bg-blue-200 w-40'>
-            <Rocket />
-            <div>icon</div>
-            <p>Total Campaigns</p>
+        <div className='bg-blue-100 w-60 p-4 rounded-2xl'>
+            {/* icons */}
+            <div className='w-12 h-12 bg-blue-600 rounded-xl 
+                    flex items-center justify-center'>
+                <Rocket 
+            size={20}
+            className="text-white w-6 h-6"
+            />
+            </div>
+            {/* details */}
+            <p className='font-bold text-gray-600'>Total Campaigns</p>
             <p>47</p>
-            <p>Active & Completed</p>
+            <p className='text-sm text-gray-500'>Active & Completed</p>
             <p>bar</p>
-            <span>percentage</span>
+            <span className='text-sm text-gray-500'>percentage</span>
         </div>
     </div>
   )
