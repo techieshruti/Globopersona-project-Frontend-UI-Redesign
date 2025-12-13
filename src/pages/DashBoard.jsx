6bb4348d26a9623dc69e4b49f19fb766198773f2
techieshruti <shrutichandra721@gmail.com>
2025-12-13 13:05:42 +0530

    added dashboard details

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Sidebar from '../components/layout/Sidebar'
+import { Bell, Search, ChevronDown } from 'lucide-react';
 
 const DashBoard = () => {
   return (
     <div>
         <Sidebar/>
-        <div className='ml-20 flex justify-around items-center'>
+        <div className='ml-20 flex justify-between items-center'>
             {/* Dashboard Content */}
             <div className='bg-gray-400'>
                 <h1 className='text-3xl font-bold p-2'>Dashboard Overview</h1>
@@ -14,7 +15,9 @@ const DashBoard = () => {
             </p>
             </div>
 
-            {/* matrics */}
+            {/* Top Right Icons */}
+            <div className='flex'>
+                {/* matrics */}
             <div className='flex gap-4 mt-6'>
                 <div>Compaign</div>
                 <div>Subscriber</div>
@@ -22,20 +25,22 @@ const DashBoard = () => {
             </div>
 
             {/* Performance Overview */}
-            <div>
+            <div className='flex items-center justify-center'>
                 <Bell />
                 <Search />
             </div>
 
             {/* admin login */}
-            <div>
+            <div className='flex items-center gap-2 border p-2 rounded-lg mr-4'>
                 <h1>R</h1>
                 <div>
                     <h2>Ravi</h2>
                 <p>Admin</p>
                 </div>
+                <ChevronDown />
             </div>
         </div>
+            </div>
     </div>
   )
 }
