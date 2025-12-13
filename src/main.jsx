7665c4aef76b7e2a3c9198727210fb8bac409abe
techieshruti <shrutichandra721@gmail.com>
2025-12-14 01:05:26 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Globopersona-project-Frontend-UI-Redesign">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
