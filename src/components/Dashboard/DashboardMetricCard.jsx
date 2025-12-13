deee16e6ea97427088798c3a5bef2f3ce3e18a11
techieshruti <shrutichandra721@gmail.com>
2025-12-13 17:12:59 +0530

    added performance overview card

--- src/components/Dashboard/DashboardMetricCard.jsx
+++ src/components/Dashboard/DashboardMetricCard.jsx
@@ -1,31 +1,43 @@
 import React from 'react'
 import { Rocket, ArrowUpRight } from "lucide-react";
 
-const DashboardMetricCard = ({ value = 85 }) => {
+const DashboardMetricCard = ({
+  title,
+  value,
+  subtitle,
+  progress,
+  trend,
+  icon,
+  bgColor,
+  barColor,
+}) => {
   return (
     <div>
-        <div className='bg-blue-50 w-1/3 p-5 rounded-2xl flex flex-col gap-2 mt-4 border-2 border-gray-100'>
-            {/* icons */}
+        <div className='${bgColor} p-5 rounded-2xl flex flex-col gap-2 mt-4 border-2 border-gray-100'>
+            
+            {/* Top row */}
             <div className='flex justify-between items-center'>
-                <div className='w-12 h-12 bg-blue-600 rounded-xl 
-                    flex items-center justify-center'>
+                <div className='w-12 h-12 rounded-xl 
+                    flex items-center justify-center ${barColor}'>
                 <Rocket 
             size={20}
             className="text-white w-6 h-6"
             />
             </div>
+
             <div className='font-bold text-green-600 flex items-center gap-1'>
                 <ArrowUpRight 
-                size={20}/>
-            <h1>+12</h1>
+                size={18}/>
+            <span>{trend}</span>
             </div>
             </div>
 
             {/* details */}
-            <p className='font-bold text-gray-500'>Total Campaigns</p>
-            <h1 className='text-3xl font-bold'>47</h1>
-            <p className='text-sm text-gray-400 font-semibold'>Active & Completed</p>
-            {/* Bar */}
+            <p className='font-bold text-gray-500'>{title}</p>
+            <h2 className='text-3xl font-bold'>{value}</h2>
+            <p className='text-sm text-gray-400 font-semibold'>{subtitle}</p>
+
+            {/* Progress Bar */}
             <div className="w-full">
       {/* Bar background */}
       <div className="w-full h-2 bg-blue-100 rounded-full overflow-hidden">
