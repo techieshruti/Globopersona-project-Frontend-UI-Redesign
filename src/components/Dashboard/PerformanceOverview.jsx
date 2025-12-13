ab3997c1f706d84b0e74d717b8c46247480a6c02
techieshruti <shrutichandra721@gmail.com>
2025-12-13 16:42:51 +0530

    added performance overview card

--- src/components/Dashboard/PerformanceOverview.jsx
+++ src/components/Dashboard/PerformanceOverview.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Rocket } from "lucide-react";
+import { Rocket, ArrowUpRight } from "lucide-react";
 
-const PerformanceOverview = () => {
+const PerformanceOverview = ({ value = 85 }) => {
   return (
     <div className='bg-[#f2f6fc] ml-10'>
         {/* Performance Overview heading */}
@@ -11,21 +11,46 @@ const PerformanceOverview = () => {
         </div>
 
         {/* Performance Overview Card */}
-        <div className='bg-blue-100 w-60 p-4 rounded-2xl'>
+        <div className='bg-blue-100 w-80 p-5 rounded-2xl flex flex-col gap-2 mt-4'>
             {/* icons */}
-            <div className='w-12 h-12 bg-blue-600 rounded-xl 
+            <div className='flex justify-between items-center'>
+                <div className='w-12 h-12 bg-blue-600 rounded-xl 
                     flex items-center justify-center'>
                 <Rocket 
             size={20}
             className="text-white w-6 h-6"
             />
             </div>
+            <div className='font-bold text-green-600 flex items-center gap-1'>
+                <ArrowUpRight 
+                size={20}/>
+            <h1>+12</h1>
+            </div>
+            </div>
+
             {/* details */}
             <p className='font-bold text-gray-600'>Total Campaigns</p>
-            <p>47</p>
-            <p className='text-sm text-gray-500'>Active & Completed</p>
-            <p>bar</p>
-            <span className='text-sm text-gray-500'>percentage</span>
+            <h1 className='text-3xl font-bold'>47</h1>
+            <p className='text-sm text-gray-400 font-semibold'>Active & Completed</p>
+            {/* Bar */}
+            <div className="w-full">
+      {/* Bar background */}
+      <div className="w-full h-2 bg-blue-100 rounded-full overflow-hidden">
+        {/* Filled bar */}
+        <div
+          className="h-full bg-blue-600 rounded-full transition-all duration-300"
+          style={{ width: `${value}%` }}
+        />
+      </div>
+
+      {/* Percentage text */}
+      <p className="mt-2 text-sm text-gray-500">
+        {value}% of target
+      </p>
+    </div>
+
+
+            <span className='text-sm text-gray-400 font-semibold'>percentage</span>
         </div>
     </div>
   )
